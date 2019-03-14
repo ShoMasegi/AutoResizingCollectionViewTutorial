b5a4fce23e628f8d1a67fbb5d29083eef7dc8ca8
ShoMasegi <m2.b0c27@gmail.com>
2019-03-15 03:41:15 +0900

    ⚙️ : Add UICollectionView Extension

---
+++ AutoResizingCollectionViewTutorial/Extensions/UICollectionView+Extension.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+extension UICollectionView {
+    func register<T: UICollectionViewCell>(_: T.Type) where T: Reusable {
+        self.register(T.self, forCellWithReuseIdentifier: T.defaultReuseIdentifier)
+    }
+    
+    func register<T: UICollectionViewCell>(_: T.Type) where T: Reusable, T: NibLoadable {
+        let bundle = Bundle(for: T.self)
+        let nib = UINib(nibName: T.nibName, bundle: bundle)
+        self.register(nib, forCellWithReuseIdentifier: T.defaultReuseIdentifier)
+    }
+    
+    func dequeueReusableCell<T: UICollectionViewCell>(forIndexPath indexPath: IndexPath) -> T where T: Reusable {
+        guard let cell = dequeueReusableCell(withReuseIdentifier: T.defaultReuseIdentifier, for: indexPath) as? T else {
+            fatalError("Could not dequeue cell with identifier: \(T.defaultReuseIdentifier)")
+        }
+        return cell
+    }
+}
+
+
+protocol Reusable: class {
+    static var defaultReuseIdentifier: String { get }
+}
+
+extension Reusable where Self: UIView {
+    static var defaultReuseIdentifier: String {
+        return String(describing: self)
+    }
+}
+
+protocol NibLoadable: class {
+    static var nibName: String { get }
+}
+
+extension NibLoadable where Self: UIView {
+    static var nibName: String {
+        return String(describing: self)
+    }
+}
