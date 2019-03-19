1906d9db232469f0d4ac145b1a5f79b058dab029
ShoMasegi <m2.b0c27@gmail.com>
2019-03-20 00:46:52 +0900

    💄 : Implement StretchImageView

--- AutoResizingCollectionViewTutorial/Components/StretchImageView.swift
+++ AutoResizingCollectionViewTutorial/Components/StretchImageView.swift
@@ -1,4 +1,16 @@
 import UIKit
 
 final class StretchImageView: UIImageView {
+    override func sizeThatFits(_ size: CGSize) -> CGSize {
+        guard let image = self.image else {
+            return super.sizeThatFits(size)
+        }
+
+        let newHeight = size.width * (image.size.height / image.size.width)
+        return CGSize(width: size.width, height: newHeight)
+    }
+
+    override var intrinsicContentSize: CGSize {
+        return sizeThatFits(bounds.size)
+    }
 }
